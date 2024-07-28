3781cd0df3dd0c8c5c7c4eaa5a7e74eb702c8018
Jaden <git@jaden.rs>
2024-07-28 02:38:57 -0500

    Set up firefox

---
+++ assets/user-overrides.js
@@ -0,0 +1,7 @@
+user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned", "");
+
+user_pref("browser.startup.page", 3);
+user_pref("browser.privatebrowsing.autostart", false);
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false);
+
+user_pref("media.ffmpeg.vaapi.enabled", true);
